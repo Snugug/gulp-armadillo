66224ce92cc0f8bd00f45ece7a8e88c6a19a3eea
Sam Richard <snugug@gmail.com>
2015-07-06 11:45:06 -0500

    Update Armadillo bubble to be less intrusive

--- helpers/armadillo.js
+++ helpers/armadillo.js
@@ -1,22 +1,37 @@
 'use strict';
 
-var chalk = require('chalk');
+var chalk = require('chalk'),
+    counter = 0;
 
 module.exports = function (text) {
-  var border = Array(text.length + 3).join('-');
+  var border = Array(text.length + 3).join('-'),
+      armadillo;
 
   //////////////////////////////
   // Armadillo ASCII Art
   //////////////////////////////
-  var armadillo =
-    chalk.red('\n               ,.-----__') +
-    chalk.red('\n            ,:::://///,:::-.') +
-    chalk.red('\n          /:\'\'/////// ``:::`;/|/') + chalk.magenta('     .' + border + '.') +
-    chalk.red('\n         /\'   ||||||     :://\'`\\') + chalk.magenta('     | ') + chalk.yellow.bold(text) + chalk.magenta(' |') +
-    chalk.red('\n        .\' ,   ||||||     `/(  ') + chalk.white('e') + chalk.red(' \\') + chalk.magenta('   /' + border + '\'') +
-    chalk.red('\n  -===~__-\'\\__X_`````\\_____/~`-._ `.') + chalk.magenta('  ') +
-    chalk.red('\n              ~~        ~~       `~-\'') + '\n';
+  if (counter === 0) {
+    armadillo =
+      chalk.red('\n               ,.-----__') +
+      chalk.red('\n            ,:::://///,:::-.') +
+      chalk.red('\n          /:\'\'/////// ``:::`;/|/') + chalk.magenta('     .' + border + '.') +
+      chalk.red('\n         /\'   ||||||     :://\'`\\') + chalk.magenta('     | ') + chalk.yellow.bold(text) + chalk.magenta(' |') +
+      chalk.red('\n        .\' ,   ||||||     `/(  ') + chalk.white('e') + chalk.red(' \\') + chalk.magenta('   /' + border + '\'') +
+      chalk.red('\n  -===~__-\'\\__X_`````\\_____/~`-._ `.') + chalk.magenta('  ') +
+      chalk.red('\n              ~~        ~~       `~-\'') + '\n';
+  }
+  else {
+    armadillo =
+      chalk.red('\n:-.') +
+      chalk.red('\n::`;/|/') + chalk.magenta('     .' + border + '.') +
+      chalk.red('\n:://\'`\\') + chalk.magenta('     | ') + chalk.yellow.bold(text) + chalk.magenta(' |') +
+      chalk.red('\n `/(  ') + chalk.white('e') + chalk.red(' \\') + chalk.magenta('   /' + border + '\'') +
+      chalk.red('\n__/~`-._ `.') + chalk.magenta('  ') +
+      chalk.red('\n~       `~-\'') + '\n';
+  }
+
 
   console.log(armadillo);
+  counter++;
   return armadillo;
 }
