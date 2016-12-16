1a3654437351bd155ce8221ebd55f74a4e7d8253
Sam Richard <sam@snug.ug>
2016-12-16 11:59:22 -0500

    :new: Update Sass task

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,66 +1,9 @@
-'use strict';
+const sass = require('../lib/tasks/sass');
 
-//////////////////////////////
-// Requires
-//////////////////////////////
-var gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    ifElse = require('gulp-if-else'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync');
-
-//////////////////////////////
-// Export
-//////////////////////////////
-module.exports = function (gulp, config) {
-  // Set value of paths to either the default or user entered
-  var SassPaths = [
-    config.folders.sass + '/**/*.sass',
-    config.folders.sass + '/**/*.scss'
-  ];
-  var sassSettings = config.options.sass;
-
-  //////////////////////////////
-  // Encapsulate task in function to choose path to work on
-  //////////////////////////////
-  var SassTask = function (path, fail) {
-    return gulp.src(SassPaths)
-      .pipe(ifElse(fail === true, function () {
-        return sass(sassSettings);
-      }, function () {
-        return sass(sassSettings).on('error', sass.logError);
-      }))
-      .pipe(autoprefixer({
-        cascade: false
-      }))
-      .pipe(gulp.dest(config.folders.server + '/' + config.folders.css + '/'))
-      .pipe(browserSync.stream());
-  }
-
-  //////////////////////////////
-  // Core Task
-  //////////////////////////////
-  gulp.task('sass', function () {
-    return SassTask(SassPaths, true);
+module.exports = gulp => {
+  gulp.task('sass', () => {
+    return gulp.src('sass/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('css'));
   });
-
-  //////////////////////////////
-  // Watch Task
-  //////////////////////////////
-  gulp.task('sass:watch', function () {
-    return gulp.watch(SassPaths)
-      .on('change', function (event) {
-        // Add absolute and relative (to Gulpfile) paths
-        event.path = {
-          absolute: event.path,
-          relative: event.path.replace(__dirname.replace('/tasks', '') + '/', '')
-        }
-
-        // Notify user of the change
-        gutil.log('File ' + gutil.colors.magenta(event.path.relative) + ' was ' + event.type);
-
-        // Call the task
-        return SassTask(event.path.absolute, false);
-      });
-  });
-}
+};
