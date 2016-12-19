4aefff87853e37a66f83ab1f8e3357c58556d8ad
Sam Richard <sam@snug.ug>
2016-12-19 12:00:00 -0500

    :white_check_mark: Add Script tests

---
+++ tests/scripts.js
@@ -0,0 +1,37 @@
+import test from 'ava';
+import map from 'map-stream';
+import {rollup, gulp} from '../lib/tasks/scripts';
+import {fromString, fromPath} from './helpers/pipe';
+
+
+test('Compiles with Node Resolve', t => {
+  const input = './tests/fixtures/js/app.js';
+  const expected = "var index=function(a,b,c){for(c=c||'0',a=a.toString();a.length<b;)a=c+a;return a};const input=document.querySelector('#number'),output=document.querySelector('#output');input.addEventListener('change',()=>{output.textContent=index(input.value,10)});\n//# sourceMappingURL=maps/app.js.map\n";
+  let contents = '';
+
+  rollup(input)
+    .pipe(map((file, cb) => {
+      contents = file.contents.toString();
+      cb(null, file);
+    }))
+    .on('error', e => {
+      t.fail(e);
+      t.end();
+    })
+    .on('end', () => {
+      t.is(contents, expected, 'Compiled and minified');
+      t.end();
+    });
+});
+
+// test('Comiles, with Gulp', t => {
+//   const input = './tests/fixtures/js/app.js';
+//   const expected = "var index=function(a,b,c){for(c=c||'0',a=a.toString();a.length<b;)a=c+a;return a};const input=document.querySelector('#number'),output=document.querySelector('#output');input.addEventListener('change',()=>{output.textContent=index(input.value,10)});\n//# sourceMappingURL=maps/app.js.map\n";
+
+//   return fromPath(input, gulp)
+//     .then(output => {
+//       console.log(output);
+
+//       t.pass();
+//     });
+// });
