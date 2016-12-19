21d290c2b9bd52abd50f5b7ac5c2da4f57abd52b
Sam Richard <sam@snug.ug>
2016-12-19 11:59:00 -0500

    :new: Add Sass Watch
    Watch plus new dest

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,9 +1,17 @@
+'use strict';
+
 const sass = require('../lib/tasks/sass');
+const config = require('config');
 
 module.exports = gulp => {
+
   gulp.task('sass', () => {
-    return gulp.src('sass/**/*.scss')
+    return gulp.src(config.watch.sass)
       .pipe(sass())
-      .pipe(gulp.dest('css'));
+      .pipe(gulp.dest(config.dest.sass));
+  });
+
+  gulp.task('sass:watch', ['sass'], () => {
+    return gulp.watch(config.watch.sass, ['sass']);
   });
 };
