c3b8a11c20881347ddf2df1046ad08eadc6849e0
Sam Richard <sam@snug.ug>
2016-12-20 12:20:01 -0500

    :white_check_mark: Add Failure, Sequence, and Task tests

---
+++ tests/failure.js
@@ -0,0 +1,51 @@
+import test from 'ava';
+
+import failure from '../lib/helpers/failure';
+
+const CI = process.env.CI;
+const FOE = process.env.FAIL_ON_ERROR;
+
+test.serial.cb('Logs out error', t => {
+  process.env.FAIL_ON_ERROR = 'false';
+
+  const context = {
+    emit: input => {
+      t.is(input, 'end', 'End emitted');
+      t.end();
+    }
+  }
+
+  failure('test').call(context, 'Test Failure Log');
+});
+
+test.serial('Fails on error, FAIL_ON_ERROR', t => {
+  process.env.FAIL_ON_ERROR = 'true';
+
+  t.throws(() => {
+    failure('test').call('Test Failure Throw - Fail on Error');
+  });
+});
+
+test.serial('Fails on error, CI', t => {
+  process.env.CI = 'true';
+
+  t.throws(() => {
+    failure('test').call('Test Failure Throw - CI');
+  });
+});
+
+test.afterEach.always('Reset Env Vars', t => {
+  if (CI === undefined)  {
+    delete process.env.CI;
+  }
+  else {
+    process.env.CI = CI;
+  }
+
+  if (FOE === undefined)  {
+    delete process.env.FAIL_ON_ERROR;
+  }
+  else {
+    process.env.FAIL_ON_ERROR = FOE;
+  }
+});
---
+++ tests/sequence.js
@@ -0,0 +1,47 @@
+import test from 'ava';
+import {build} from '../lib/helpers/sequence';
+
+test('Build from Array', t => {
+  const cb = (error) => error;
+  const input = [
+    'sass',
+    'js'
+  ];
+
+  const expected = input;
+  expected.push(cb);
+
+  const output = build(input, cb);
+
+  t.deepEqual(output, expected, 'CB is added to input array');
+});
+
+test('Build from Object', t => {
+  const cb = (error) => error;
+  const input = {
+    clean: [
+      'clean',
+      'clear',
+    ],
+    build: [
+      'sass',
+      'js',
+    ],
+  }
+
+  const expected = [
+    [
+      'clean',
+      'clear',
+    ],
+    [
+      'sass',
+      'js',
+    ],
+    cb,
+  ];
+
+  const output = build(input, cb);
+
+  t.deepEqual(output, expected, 'CB is added to input array');
+});
