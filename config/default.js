679037e5bf4c4e44263bd6df0666e27434989aec
Sam Richard <sam@snug.ug>
2016-12-20 12:20:46 -0500

    :art: Move failure check in to failure helper

--- config/default.js
+++ config/default.js
@@ -3,12 +3,6 @@
 const rollupNode = require('rollup-plugin-node-resolve');
 const rollupCommon = require('rollup-plugin-commonjs');
 
-let fail = false;
-
-if (process.env.FAIL_ON_ERROR === 'true' || process.env.CI === 'true') {
-  fail = true;
-}
-
 module.exports = {
   rollup: {
     entry: 'js/main.js',
@@ -32,7 +26,6 @@ module.exports = {
     enable: true,
     directory: '../maps',
   },
-  failOnError: fail,
   watch: {
     sass: 'sass/**/*.scss',
     js: 'js/**/*.js',
