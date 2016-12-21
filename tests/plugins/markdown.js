2ab9cc689d2c32c15565ea8a46d5ad8841ea5942
Sam Richard <sam@snug.ug>
2016-12-21 12:08:29 -0500

    :white_check_mark: Add Custom Plugin and No Compile
    * Make sure custom plugins are used when rendering Markdown
    * Add test for null file
    * Add test for streams that throw errors

--- tests/plugins/markdown.js
+++ tests/plugins/markdown.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import {fromString} from '../helpers/pipe';
+import {fromString, fromNull, fromStream} from '../helpers/pipe';
 import markdown from '../../lib/plugins/markdown';
 
 test('Compiles Markdown - .md', t => {
@@ -22,6 +22,17 @@ test('Compiles Markdown - .markdown', t => {
     });
 });
 
+test('Compiles Markdown with Custom Plugin', t => {
+  const input = '@[Taylor Swift - Shake it Off](https://www.youtube.com/watch?v=nfWlot6h_JM)';
+  const expected = `<p><div class=\"flexible-video\"><iframe  src=\"https://www.youtube.com/embed/nfWlot6h_JM\" width=\"560\" height=\"315\" frameborder=\"0\" allowfullscreen></iframe></div></p>\n`;
+
+  return fromString(input, 'shake/it/off.md', markdown)
+    .then(output => {
+      t.is(output, expected, 'Markdown compiles as expected');
+    });
+});
+
+
 test('No Compile - .html', t => {
   const input = '# Hello World';
   const expected = '# Hello World';
@@ -30,4 +41,15 @@ test('No Compile - .html', t => {
     .then(output => {
       t.is(output, expected, 'Markdown compiles as expected');
     });
+});
+
+test('No Compile - null', t => {
+  return fromNull(markdown)
+    .then(output => {
+      t.is(output, '', 'No output');
+    });
+});
+
+test('Error - is stream', t => {
+  t.throws(fromStream(markdown));
 });
