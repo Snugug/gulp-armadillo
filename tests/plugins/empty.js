166e0393207e6ec62fbab865e3e04089f834eba2
Sam Richard <sam@snug.ug>
2016-12-28 13:48:50 -0500

    :white_check_mark: Add Empty plugin tests

---
+++ tests/plugins/empty.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import {fromString} from '../helpers/pipe';
+import plugin from '../helpers/plugin';
+import empty from '../../lib/plugins/empty';
+
+test('No error on full file', t => {
+  t.notThrows(fromString('Test', 'pages/empty', empty));
+})
+
+test('Errors out on empty file', t => {
+  t.throws(fromString('', 'pages/empty', empty));
+});
+
+plugin(empty, test);
