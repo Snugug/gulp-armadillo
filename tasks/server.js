0881934fea202271e8b0412a3dfcac31447e5d3e
Sam Richard <sam@snug.ug>
2016-12-28 12:05:42 -0500

    :memo: Armadillo talks now!

--- tasks/server.js
+++ tasks/server.js
@@ -3,6 +3,7 @@
 const config = require('config');
 
 const sequence = require('../lib/helpers/sequence');
+const armadillo = require('../lib/helpers/armadillo');
 const browserSync = require('browser-sync');
 
 module.exports = gulp => {
@@ -15,6 +16,7 @@ module.exports = gulp => {
   });
 
   gulp.task('serve', 'Compiles and serves the static site, watching for changes, and recompiling the site on change', cb => {
+    armadillo('Serving');
     return sequence(config.tasks.serve, cb);
   });
 }
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const config = require('config');
+const sequence = require('../lib/helpers/sequence');
+const armadillo = require('../lib/helpers/armadillo');
 
 module.exports = gulp => {
-  gulp.task('watch', 'Runs all watch tasks', config.tasks.watch);
+  gulp.task('watch', 'Runs all watch tasks', cb => {
+    armadillo('Watching');
+    return sequence(config.tasks.watch, cb);
+  });
 }
